550c8f7012b84d9c292cb91ee923219065df4d9d
Marcelo Santos <rocha.mer21@gmail.com>
2022-04-17 23:00:30 -0300

    contando total de letras no arquivo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,23 @@ import java.io.File
 
 fun main() {
     var count = 0
-    val fileName = "src/words_with_numbers.txt"
-    val lines = File(fileName).readLines().forEach { if (it.first().isDigit()) count++ }
-    print(count)
+    //val fileName = "src/words_with_numbers.txt"
+    val wordsSequence = "src/words_sequence.txt"
+    val wordsNumbers = "src/text.txt"
+
+    //File(fileName).readLines().forEach { if (it.first().isDigit()) count++ }
+    //print(count)
+
+//    val line = File(wordsSequence).readLines()
+//    var temp: String = line[0]
+//
+//    for (i in line.indices) {
+//        if (temp.length < line[i].length) temp = line[i]
+//        //println(line[i])
+//    }
+//
+    val line = File(wordsNumbers).readText().split(" ").size
+    print(line)
+
+
 }
